d1cc17048d2eb7b0455d72936f5d8fb5b7f67a98
Jennifer Bouquin <jennifer.bouquin@gmail.com>
2021-11-01 14:54:03 +0100

    feat: :sparkles: Add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+CURRENT_DIR=$(pwd)
+
+DOTFILES_PROJECT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+unlink ~/.zshrc
+ln -sv $DOTFILES_PROJECT_PATH/zsh/.zshrc ~
+
+unlink ~/.p10k.zsh
+ln -sv $DOTFILES_PROJECT_PATH/zsh/.p10k.zsh ~
+
+unlink ~/.gitconfig
+ln -sv $DOTFILES_PROJECT_PATH/git/.gitconfig ~
